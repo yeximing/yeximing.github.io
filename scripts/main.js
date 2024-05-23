d9a4c8c2fa886312e1f1288ffbf17d08c4f0da9b
yeximing <l912341813@gmail.com>
2024-05-23 18:00:53 +0800

    add yuanshen title pic

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,34 @@
 let myImage = document.querySelector("img");
 
 myImage.onclick = function () {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/noell.jpg") {
-    myImage.setAttribute("src", "images/hutao.jpg");
-  } else {
-    myImage.setAttribute("src", "images/noell.jpg");
-  }
+	let mySrc = myImage.getAttribute("src");
+	if (mySrc === "images/noell.jpg") {
+		myImage.setAttribute("src", "images/hutao.jpg");
+	} else {
+		myImage.setAttribute("src", "images/noell.jpg");
+	}
 };
 
 let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+let myHeading = document.querySelector("h2");
 
 function setUserName() {
-  let myName = prompt("请输入你的名字。");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = "原神，" + myName;
-  }
+	let myName = prompt("请输入你的名字。");
+	if (!myName) {
+		setUserName();
+	} else {
+		localStorage.setItem("name", myName);
+		myHeading.textContent = myName + ",我要启动";
+	}
 }
 
 if (!localStorage.getItem("name")) {
-  setUserName();
+	setUserName();
 } else {
-  let storedName = localStorage.getItem("name");
-  myHeading.textContent = "原神，" + storedName;
+	let storedName = localStorage.getItem("name");
+	myHeading.textContent = storedName + ",我要启动";
 }
 
 myButton.onclick = function () {
-  setUserName();
+	setUserName();
 };
